e2fa8c35b66882a5e4724ee3dbfdc2d1d1f390e7
Fahid Javid <fahidjavid@gmail.com>
2024-08-18 15:22:15 +0500

    Moved packages section to partials.

--- index.php
+++ index.php
@@ -20,70 +20,8 @@
     <div id="page-wrap">
 
     <?php
-        $heading = "Pricing";
-        $prices =[
-            [
-                'title' => "Basic",
-                'head' =>   "$200",
-                'button' => "Buy Now",
-                'class'  => "basic",
-                'classes' => "",
-               
-                'list' => array("Landing Page","Visual Branding")
-               
-            ],
-            [
-                'title' => "Standard",
-                'head' =>   "$500",
-                'button' => "Buy Now",
-                'class'  => "standard",
-                'classes' => "ribbon ribbon-top-right",
-                'list' => array("Landing Page","Visual Branding","Web design")
-                
-            ],
-            [
-                'title' => "Premium",
-                'head' =>   "$1k",
-                'button' => "Buy Now",
-                'class'  => "premium",
-                'classes' => "",
-                'list' => array("Landing Page","Visual Branding","Web design","Market Research")
-
-            ]
-        ];
+        require_once "partials/packages.php";
     ?>
-        <!-- pricing section -->
-     	<div id="pricing-section" class="page-section">
-            <h2><?php echo $heading; ?></h2>
-            <div class="pricing-table">
-                
-                    <?php 
-                    foreach($prices as $price){
-                    ?>
-                    <div class="pricing <?php echo $price['class'] ?>">
-                        <div class="<?php echo $price['classes'] ?>"><span></span></div>
-                        <h3><?php echo $price['title']; ?></h3>
-                        <hr>
-                        <h2><?php echo $price['head']; ?></h2>
-         				<ul>
-                            <?php
-                            foreach ($price['list'] as $item) {
-                            ?>
-         					<li><i class="fa-solid fa-circle-check"></i> <?php echo $item; ?></li>
-                            <?php
-                                } 
-                            ?>
-         				</ul>
-         				<div class="package-btn">
-         					<a><?php echo $price['button']; ?></a>
-         				</div>
-                    </div>
-                    <?php
-                    }
-                    ?>
-
-            </div><!--  closing pricing-table -->
-        </div> <!-- closing pricing-section -->
 
     </div> <!-- closing page wrapper -->
 </body>
